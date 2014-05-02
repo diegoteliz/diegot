d6600a61d5e166c5dc70059ba64b0749c3e872a2
Diego <contacto@diegoteliz.com>
2014-05-02 16:15:30 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<title>Diego Téliz</title>
+		<meta name="description" content="Sitio oficial de Diego Téliz" />
+		<meta name="author" content="Diego Teliz" />
+		
+		<!-- Styles & Fonts-->
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+		
+		<!-- Scripts -->
+		<script data-main="js/main" src="js/lib/require.js"></script>
+	</head>
+	<body>
+		<header id="main-menu">
+			<!-- <a href="/" id="logo">
+				<img src="" alt="Diego Téliz">
+			</a> -->
+		</header>
+		<div id="overlay"></div>
+		<div id="dinamic-content">
+			<section id="work">
+				<article id="work-item-1" class="work-item"></article>
+				<article id="work-item-2" class="work-item"></article>
+				<article id="work-item-3" class="work-item"></article>
+				<article id="work-item-4" class="work-item"></article>
+				<article id="work-item-5" class="work-item"></article>
+			</section>
+		</div>
+	</body>
+</html>
