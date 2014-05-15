1d18d453469e23dc0d68df798ed4eab0e7793df1
Diego <contacto@diegoteliz.com>
2014-05-15 01:21:19 -0300

    update

--- template/js/work.js
+++ template/js/work.js
@@ -3,12 +3,19 @@ require([
 ], function(backstretch){
 
 	//$('#work-item-2').backstretch(['/img/work/escuela-plus.jpg'],{centeredY:false});
+	$('#work-item-1').backstretch(['/img/work/takeoff.jpg'],{centeredY:false});
+	$('#work-item-1').css({backgroundColor:'#333333'});
+
 	$('#work-item-2').backstretch('/img/work/pago-movil.jpg');
 	$('#work-item-2').css({backgroundColor:'#333333'});
 
-	$('#work-item-4').backstretch('/img/work/escuela-plus.jpg');
+	$('#work-item-3').backstretch('/img/work/petersen.jpg');
+	$('#work-item-3').css({backgroundColor:'#333333'});
+
+	$('#work-item-4').backstretch(['/img/work/escuela-plus.jpg'],{centeredY:false});
 	$('#work-item-4').css({backgroundColor:'#333333'});
 
-	$('#work-item-1, #work-item-3, #work-item-5').backstretch('/img/work/pago-movil.jpg');
+	$('#work-item-5').backstretch('/img/work/punta-carretas-shopping.jpg');
+	$('#work-item-5').css({backgroundColor:'#333333'});
 
 });
