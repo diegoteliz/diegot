44abaf4b23471aa8542bdbdac012a367a9a70920
Diego <contacto@diegoteliz.com>
2014-05-23 21:25:16 -0300

    update

--- template/js/work.js
+++ template/js/work.js
@@ -2,19 +2,19 @@ require([
 	'lib/jquery.backstretch'
 ], function(backstretch){
 
-	$('#work-item-1 a figure.work-img').backstretch(['/img/work/takeoff.jpg'],{centeredY:false});
+	$('#work-item-1 a figure .work-img').backstretch(['/img/work/takeoff.jpg'],{centeredY:false});
 	$('#work-item-1').css({backgroundColor:'#333333'});
 
-	$('#work-item-2 a figure.work-img').backstretch('/img/work/petersen.jpg');
+	$('#work-item-2 a figure .work-img').backstretch('/img/work/petersen.jpg');
 	$('#work-item-2').css({backgroundColor:'#333333'});
 
-	$('#work-item-3 a figure.work-img').backstretch(['/img/work/punta-carretas-shopping.jpg'],{centeredY:false});
+	$('#work-item-3 a figure .work-img').backstretch(['/img/work/punta-carretas-shopping.jpg'],{centeredY:false});
 	$('#work-item-3').css({backgroundColor:'#333333'});
 
-	$('#work-item-4 a figure.work-img').backstretch(['/img/work/escuela-plus.jpg'],{centeredY:false});
+	$('#work-item-4 a figure .work-img').backstretch(['/img/work/escuela-plus.jpg'],{centeredY:false});
 	$('#work-item-4').css({backgroundColor:'#333333'});
 
-	$('#work-item-5 a figure.work-img').backstretch('/img/work/lactld.jpg');
+	$('#work-item-5 a figure .work-img').backstretch('/img/work/lactld.jpg');
 	$('#work-item-5').css({backgroundColor:'#333333'});
 
 });
