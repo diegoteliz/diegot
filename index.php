e7608a6bf6460f0e33b349287a58ce42e7926c83
Diego <contacto@diegoteliz.com>
2014-11-15 00:41:22 -0200

    Update for about section

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<title>Diego Téliz</title>
 		<meta name="description" content="Official Diego Téliz website" />
 		<meta name="author" content="Diego Teliz" />
-		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<!--
 
 		                    ░▓██      
--- template/about.php
+++ template/about.php
@@ -10,17 +10,17 @@
 					<div class="separator"></div>
 				</figcaption>
 			</figure>
-			<p>I'm Diego, a <?php echo get_years_old( '1991-03-24' ) ?> years old web developer and designer since <?php echo get_experience_years( '2007' ) ?> years. I also have experience in mobile application development, 3D modeling, animation, networking and virtualization. Currently live in Uruguay and work as a Senior Web Developer at <a href="http://takeoffmedia.com/" rel="nofollow" target="_blank">Takeoff Media</a> and I manage a personal venture: <a href="http://aero.com.uy/" target="_blank">Aero Creative Studio</a>.</p>
-			<p>When I working I give much importance to details, even the smallest. I'm always researching and learning new technologies. My goal with each project is always to make a functional, user-friendly and innovative product. I love what I do and try to beat myself everyday.</p>
+			<p>I'm Diego, a <?php echo get_years_old( '1991-03-24' ) ?> years old web developer and designer with <?php echo get_experience_years( '2007' ) ?> years of experience. I also have experience in mobile application development, 3D modeling, animation, networking and virtualization. Currently live in Uruguay and work as Web UI Developer for <a href="http://www.nationalgeographic.com/" rel="nofollow" target="_blank">National Geographic</a> at <a href="http://www.globant.com/" rel="nofollow" target="_blank">Globant</a> and I manage a personal venture: <a href="http://aero.com.uy/" target="_blank">Aero Creative Studio</a>.</p>
+			<p>When I'm working I give much importance to quality and details. I'm always researching and learning new technologies. My goal with each project is make a functional, user-friendly and innovative product. I love what I do and try to beat myself everyday.</p>
 		</section>
 		<section id="skills">
 			<h1>Skills</h1>
 			<h2>"Yoda Master" Level</h2>
-			<p>HTML5 / CSS3 / JavaScript / PHP / jQuery / jQuery Mobile / Responsive Design / Fireworks / UI &amp; UX /  WordPress</p>
+			<p>HTML5 / CSS3 / JavaScript / PHP / jQuery / jQuery Mobile / Sass / Responsive Design / Fireworks / UI &amp; UX /  WordPress</p>
 			<h2>"Obi-Wan" Level</h2>
-			<p>Backbone.js / Sass / MySQL / JSON / PhoneGap / Prestashop / Illustrator / Flash and ActionScript / Photoshop / SEO</p>
+			<p>Backbone.js / Grunt / Handlebars / Bootstrap / MySQL / PhoneGap / Prestashop / Illustrator / Flash / ActionScript / Photoshop / SEO</p>
 			<h2>"Anakin Skywalker" Level</h2>
-			<p>Node.js / Ghost CMS / KeystoneJS CMS / Angular / <br />
+			<p>CoffeeScript / Node.js / Ghost CMS / KeystoneJS CMS / Angular / <br />
 				Game development with Construt and Unity</p>
 			<h2>Languages</h2>
 			<p>Spanish (native) / English (intermediate) / Portuguese (intermediate)</p>
