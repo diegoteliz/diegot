2e663f1db9b70ead563f7cadf82d65f3e88dc89a
diegoteliz <diegoteliz007@gmail.com>
2015-07-11 23:45:20 -0300

    Contact - Titles added to social links

--- template/contact.php
+++ template/contact.php
@@ -1,11 +1,11 @@
 <section id="contact">
 	<nav id="social-links-wrapper">
 		<ul id="social-links">
-			<li><a href="http://www.linkedin.com/in/diegoteliz" class="social-link icon-linkedin" id="linkedin" rel="nofollow" target="_blank"></a></li>
-			<li><a href="http://twitter.com/diegoteliz" class="social-link icon-twitter" id="twitter" rel="nofollow" target="_blank"></a></li>
-			<li><a href="mailto:contacto@diegoteliz.com" class="social-link icon-email" id="email"></a></li>
-			<li><a href="https://www.facebook.com/diegoteliz" class="social-link icon-facebook" id="facebook" rel="nofollow" target="_blank"></a></li>
-			<li><a href="https://github.com/diegoteliz" class="social-link icon-github" id="github" rel="nofollow" target="_blank"></a></li>
+			<li><a href="http://www.linkedin.com/in/diegoteliz" title="Linkedin" class="social-link icon-linkedin" id="linkedin" rel="nofollow" target="_blank"></a></li>
+			<li><a href="http://twitter.com/diegoteliz" title="Twitter" class="social-link icon-twitter" id="twitter" rel="nofollow" target="_blank"></a></li>
+			<li><a href="mailto:contacto@diegoteliz.com" title="Email" class="social-link icon-email" id="email"></a></li>
+			<li><a href="https://www.facebook.com/diegoteliz" title="Facebook" class="social-link icon-facebook" id="facebook" rel="nofollow" target="_blank"></a></li>
+			<li><a href="https://github.com/diegoteliz" title="Github" class="social-link icon-github" id="github" rel="nofollow" target="_blank"></a></li>
 		</ul>
 	</nav>
 	<div id="contact-info">
