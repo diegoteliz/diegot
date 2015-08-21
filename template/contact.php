f68fda1828d2a79c79894c10512c3f9da08bbb0b
diegoteliz <diegoteliz007@gmail.com>
2015-08-21 22:56:48 +1200

    Personal data updated

--- template/contact.php
+++ template/contact.php
@@ -3,7 +3,7 @@
 		<ul id="social-links">
 			<li><a href="http://www.linkedin.com/in/diegoteliz" title="Linkedin" class="social-link icon-linkedin" id="linkedin" rel="nofollow" target="_blank"></a></li>
 			<li><a href="http://twitter.com/diegoteliz" title="Twitter" class="social-link icon-twitter" id="twitter" rel="nofollow" target="_blank"></a></li>
-			<li><a href="mailto:contacto@diegoteliz.com" title="Email" class="social-link icon-email" id="email"></a></li>
+			<li><a href="mailto:aloha@diegoteliz.com" title="Email" class="social-link icon-email" id="email"></a></li>
 			<li><a href="https://www.facebook.com/diegoteliz" title="Facebook" class="social-link icon-facebook" id="facebook" rel="nofollow" target="_blank"></a></li>
 			<li><a href="https://github.com/diegoteliz" title="Github" class="social-link icon-github" id="github" rel="nofollow" target="_blank"></a></li>
 		</ul>
@@ -12,8 +12,8 @@
 		<p>
 			<b>Diego J. Téliz</b> <br />
 			<b>Sr. Web Developer</b> <br />
-			+598 95 938 477 <br />
-			<a href="mailto:contacto@diegoteliz.com">contacto@diegoteliz.com</a>
+			+64 21 638 787 <br />
+			<a href="mailto:aloha@diegoteliz.com">aloha@diegoteliz.com</a>
 		</p>
 	</div>
 </section>
