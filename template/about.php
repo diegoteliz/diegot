7a7e2fa7b243445f7aace141d03b7bc8a5543820
Diego Teliz <diegoteliz007@gmail.com>
2016-05-06 13:46:43 +1200

    Minor changes on About section

--- template/about.php
+++ template/about.php
@@ -6,15 +6,15 @@
 				<img src="/img/diego-teliz.jpg" alt="." />
 				<figcaption>
 					<h1>Diego J. Téliz</h1>
-					<h2><b>Sr. Web Developer</b></h2>
+					<h2><b>Full-Stack Developer</b></h2>
 					<div class="separator-small"></div>
 				</figcaption>
 			</figure>
-			<p>I'm Diego, a <?php echo get_years_old( '1991-03-24' ) ?> years old web developer and designer with <?php echo get_experience_years( '2008' ) ?> years of experience. I also have experience in mobile application development, 3D modeling, animation, networking and virtualization. I was born in Uruguay but I'm currently living in Auckland, New Zealand and working as Web UI Developer at <a href="http://www.nationalgeographic.com/" rel="nofollow" target="_blank">National Geographic</a>.</p>
+			<p>I'm Diego, a <?php echo get_years_old( '1991-03-24' ) ?> years old web developer and designer with <?php echo get_experience_years( '2008' ) ?> years of experience. I also have experience in mobile application development, 3D modeling, animation, networking and virtualization. I was born in Uruguay but I'm currently living in Auckland, New Zealand and working as Full-Stack Developer.</p>
 			<p>When I'm working I give much importance to quality and details. I'm always researching and learning new technologies. My goal in every project is make a functional, user-friendly and innovative product. I love what I do and try to beat myself everyday.</p>
 		</section>
 		<section id="skills">
-			<h1>Skills</h1>
+			<h1>Skill set</h1>
 			<h2>"Yoda Master" Level</h2>
 			<p>HTML5 / CSS3 / JavaScript / WordPress / Backbone.js / jQuery / Sass / Responsive Design / Fireworks / UI &amp; UX </p>
 			<h2>"Obi-Wan" Level</h2>
@@ -25,7 +25,7 @@
 			<p>Spanish (native) / English (intermediate) / Portuguese (intermediate)</p>
 		</section>
 		<section id="brands">
-			<h1>I worked for</h1>
+			<h1>I've worked for</h1>
 			<ul id="brands-list"><!--
 				--><li><img src="/img/brand/1.png" alt="National Geographic" title="National Geographic" /></li><!--
 				--><li><img src="/img/brand/2.png" alt="Visa" title="Visa" /></li><!--
