3a43b9bb4b1fb12462eb92bcd2c3aedb69c3bc37
Diego Teliz <diegoteliz007@gmail.com>
2017-08-14 12:42:36 +1200

    Fix minor error in description

--- template/about.php
+++ template/about.php
@@ -10,8 +10,8 @@
 					<div class="separator-small"></div>
 				</figcaption>
 			</figure>
-			<p>I'm Diego, a <?php echo get_years_old( '1991-03-24' ) ?> years old web developer and designer with <?php echo get_experience_years( '2008' ) ?> years of experience. I also have experience in mobile application development, 3D modeling, animation, networking and virtualization. I was born in Uruguay but I'm currently living in Auckland, New Zealand and working as Full-Stack Developer.</p>
-			<p>When I'm working I give much importance to quality and details. I'm always researching and learning new technologies. My goal in every project is make a functional, user-friendly and innovative product. I love what I do and try to beat myself everyday.</p>
+			<p>I'm Diego, a <?php echo get_years_old( '1991-03-24' ) ?> years old web developer and designer with <?php echo get_experience_years( '2008' ) ?> years of experience. I also have experience in mobile application development, 3D modeling, animation, networking and virtualization. I was born in Uruguay and I'm currently living in Auckland, New Zealand and working as Full-Stack Developer.</p>
+			<p>When I'm working I give much importance to quality and details. I'm always researching and learning new technologies. My goal in every project is make a functional, user-friendly and innovative product. I love what I do and try to exceed myself everyday.</p>
 		</section>
 		<section id="skills">
 			<h1>Skill set</h1>
