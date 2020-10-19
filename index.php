d84b593786f98385065a8f767f0062cd82f8d02c
Diego Teliz <diegoteliz007@gmail.com>
2020-10-19 13:35:49 +1300

    Update GA Google Analytics snippet

--- index.php
+++ index.php
@@ -36,6 +36,16 @@
 		Code and Design by Diego Teliz - aloha@diegoteliz.com (@DiegoTeliz)
 
 		-->
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-36939365-1"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-36939365-1');
+		</script>
+
 		
 		<!-- Styles & Fonts-->
 		<link rel="stylesheet" type="text/css" href="/css/style.css?ver=1.1" />
@@ -62,25 +72,6 @@
 		<meta name="theme-color" content="#006F6B">
 		<meta name="msapplication-navbutton-color" content="#006F6B">
 		<meta name="apple-mobile-web-app-status-bar-style" content="#006F6B">
-		
-		<!-- Anti-flicker snippet -->
-		<style>.async-hide { opacity: 0 !important} </style>
-		<script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-		h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-		(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-		})(window,document.documentElement,'async-hide','dataLayer',4000,
-		{'GTM-5Q73Q8L':true});</script>
-		<!-- Google Analytics -->
-		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-			ga('create', 'UA-36939365-1', 'diegoteliz.com');
-			ga('require', 'GTM-5Q73Q8L');
-			ga('send', 'pageview');
-		</script>
 	</head>
 	<body>
 		<header id="main-menu">
